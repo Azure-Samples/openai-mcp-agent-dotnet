818367781d2d7548d112b6552779e279c8563606
Justin Yoo <justin.yoojh@gmail.com>
2025-08-08 22:38:14 +0900

    Update the static property readonly

--- src/McpTodo.ClientApp/Constants.cs
+++ src/McpTodo.ClientApp/Constants.cs
@@ -2,5 +2,5 @@ namespace McpTodo.ClientApp;
 
 public class Constants
 {
-    public static List<string> GitHubModelEndpoints { get; set; } = ["https://models.inference.ai.azure.com", "https://models.github.ai/inference"];
+    public static List<string> GitHubModelEndpoints { get; } = ["https://models.inference.ai.azure.com", "https://models.github.ai/inference"];
 }
